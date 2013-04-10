c8bb11338e4ea336b8baddb7d24d7bb12a79364a
Jared Beck <jared@singlebrook.com>
2013-04-10 15:53:14 -0400

    more parsing steps

--- bin/rails/parse_log.sh
+++ bin/rails/parse_log.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 set -e
 
+# Inventory routes by processing a production log file
+
 # Remove lines that don't begin with ..
 egrep '(Processing|Completed)' production.log > 2.log
 
@@ -9,7 +11,16 @@ egrep '(Processing|Completed)' production.log > 2.log
 cat 2.log | tr "\n" '$' | sed 's/$Completed//g' | tr '$' "\n" > 3.log
 
 # Project only the fields we want
-cat 3.log | sed -E 's/(Processing )([[:alnum:]:]+#[[:alnum:]]+)(.*)(\[[A-Z]+])([^[]*)(\[http.*\])/\2 \4 \6/g' > 4.log
+cat 3.log | sed -E 's/(Processing )([[:alnum:]:]+#[[:alnum:]_]+)(.*)(\[[A-Z]+])([^[]*)(\[http.*\])/\2 \4 \6/g' > 4.log
 
 # Get distinct lines
 cat 4.log | sort | uniq > 5.log
+
+# Replace numbers in URL with ":n" to indicate dynamic sement
+cat 5.log | sed -E 's/\/[0-9]+\//\/:n\//g' | sort | uniq > 6.log
+
+# www.ggchapters.org is the same as ggchapters.org
+cat 6.log | sed 's/www.ggchapters.org/ggchapters.org/g' | sort | uniq > 7.log
+
+# Replace numbers at the ends of URLs with ":n" to indicate dynamic segment
+cat 7.log | sed -E 's/\/[0-9]*]/\/:n]/g' | sort | uniq > 8.log
