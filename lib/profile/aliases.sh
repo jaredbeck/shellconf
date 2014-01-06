c111f4b98f298040b680ba054cb1a1c6cafe3c52
Jared Beck <jared@jaredbeck.com>
2014-01-06 13:39:45 -0500

    cuke alias

--- lib/profile/aliases.sh
+++ lib/profile/aliases.sh
@@ -23,6 +23,7 @@ alias usejava8='export JAVA_HOME=$(/usr/libexec/java_home -v 1.8);PATH=${JAVA_HO
 
 # Aliases: Ruby dev
 alias be='bundle exec'
+alias cuke='bundle exec cucumber'
 alias startwork='git pull --rebase && git submodule update --init && bundle update && be rake'
 
 # Aliases: Dwarf Fortress
