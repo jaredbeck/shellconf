e60628b7462dc726da20cd0b2d401461443468a8
Jared Beck <jared@jaredbeck.com>
2014-05-08 18:11:06 -0400

    Offer to delete old branches

--- bin/branch_cherries.rb
+++ bin/branch_cherries.rb
@@ -1,22 +1,63 @@
 #!/usr/bin/env ruby
 
+require 'set'
+
 FMT = '%d %s'
 LINE_SEP = "\n"
 
-def local_branches
-  `git for-each-ref --format='%(refname)' refs/heads/ |
-  cut -d '/' -f 3`
-  .split(LINE_SEP)
+class BranchCherries
+
+  attr_reader :branches
+
+  def initialize
+    @branches = branch_commits_ahead
+  end
+
+  def run
+    puts '%d branches have commits not in master' % [ahead.size]
+    puts '%d branches can be deleted.' % [deleteable.size]
+    unless deleteable.empty?
+      puts 'Deleteable: '
+      deleteable.each { |br| puts '  ' + br }
+      print 'Delete? [Y/n]: '
+      choice = gets.chomp
+      deleteable.each { |br| delete(br) } if choice == 'Y'
+    end
+  end
+
+private
+
+  def ahead
+    branches.reject { |br, n| n.zero? }
+  end
+
+  def deleteable
+    @_deleteable ||= branches.select { |br, n| n.zero? }.keys
+  end
+
+  def delete(branch)
+    system('git branch --delete "%s" > /dev/null' % [branch])
+  end
+
+  def local_branch_array
+    `git for-each-ref --format='%(refname)' refs/heads/ |
+    cut -d '/' -f 3`
+    .split(LINE_SEP)
+  end
+
+  def local_branches
+    Set.new(local_branch_array).delete('master')
+  end
+
+  def commits_ahead(branch)
+    `git cherry master #{branch} |
+    wc -l`
+    .to_i
+  end
+
+  def branch_commits_ahead
+    local_branches.inject({}) { |hsh, br| hsh.update(br => commits_ahead(br)) }
+  end
 end
 
-def commits_ahead(branch)
-  `git cherry master #{branch} |
-  wc -l`
-  .to_i
-end
-
-def main
-  local_branches.each { |b| puts FMT % [commits_ahead(b), b] }
-end
-
-main
+BranchCherries.new.run
