66e311ede93fb9ae65959d07a13f4b2f54946129
Jared Beck <jared@jaredbeck.com>
2014-11-02 14:29:25 -0500

    Add functions for listing available gem versions

--- lib/profile/aliases/ruby.sh
+++ lib/profile/aliases/ruby.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+source "$SHELLCONF_PATH/lib/jb_gem_versions.sh"
+
 alias be='bundle exec'
 alias brs='bin/rails server'
 alias brc='bin/rails console'
+alias gem_versions=jb_gem_versions
