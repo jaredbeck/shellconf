69784e92310ef6e0cce8c156fb24b0f5f6eeb633
Jared Beck <jared@jaredbeck.com>
2016-11-04 14:35:33 -0400

    Add alias for npmup

--- lib/profile/aliases.sh
+++ lib/profile/aliases.sh
@@ -3,6 +3,7 @@
 source "$SHELLCONF_PATH/lib/profile/aliases/games.sh"
 source "$SHELLCONF_PATH/lib/profile/aliases/heroku.sh"
 source "$SHELLCONF_PATH/lib/profile/aliases/java.sh"
+source "$SHELLCONF_PATH/lib/profile/aliases/js.sh"
 source "$SHELLCONF_PATH/lib/profile/aliases/miscelaneous.sh"
 source "$SHELLCONF_PATH/lib/profile/aliases/ruby.sh"
 source "$SHELLCONF_PATH/lib/profile/aliases/version_control.sh"
---
+++ lib/profile/aliases/js.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+alias npmup="$SHELLCONF_PATH/bin/js/npmup.rb"
