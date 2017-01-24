08d1350518dc0c13a1204cc39971a378b5394fe2
Jared Beck <jared@jaredbeck.com>
2017-01-24 15:59:23 -0500

    Add usage instructions

--- bin/ruby/gitcov.sh
+++ bin/ruby/gitcov.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+
+# Usage: gitcov.sh [commit]
+#
+# Generate coverage report with
+# [simplecov-csv](https://github.com/fguillen/simplecov-csv)
+
 set -e
 COVERAGE_REPORT='coverage/results.csv'
 SHA=$1
