c971bc6beb25c15f42dce5d12076a47460a6a190
Jared Beck <jared@jaredbeck.com>
2021-06-30 15:40:19 -0400

    Timestamp aliases

--- lib/profile/aliases/miscelaneous.sh
+++ lib/profile/aliases/miscelaneous.sh
@@ -3,6 +3,11 @@
 alias l="clear; ls -l"
 alias cdl="cd \!^; clear; ls -l"
 alias abspath="$SHELLCONF_PATH/bin/abspath.rb"
-alias timestamp='date "+%Y%m%d%H%M%S"'
 alias isodate='date "+%Y-%m-%d"'
 alias lipsum='ruby -r faker -e "puts Faker::Lorem.paragraph" | pbcopy'
+
+# Timestamp (TS) - commonly used for DCS release titles
+alias ts='date "+%Y-%m-%d %H:%M %Z"'
+
+# Timestamp No Delimiter (ND) - rarely used
+alias tsnd='date "+%Y%m%d%H%M%S"'
