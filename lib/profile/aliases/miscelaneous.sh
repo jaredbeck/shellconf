362403779ef685397061cac4d4d0a28af4597bc3
Jared Beck <jared@jaredbeck.com>
2024-01-02 16:51:06 -0500

    Aliases: pbsort, timestamps

--- lib/profile/aliases/miscelaneous.sh
+++ lib/profile/aliases/miscelaneous.sh
@@ -5,9 +5,9 @@ alias cdl="cd \!^; clear; ls -l"
 alias abspath="$SHELLCONF_PATH/bin/abspath.rb"
 alias isodate='date "+%Y-%m-%d"'
 alias lipsum='ruby -r faker -e "puts Faker::Lorem.paragraph" | pbcopy'
+alias pbsort='pbpaste | sort | pbcopy'
 
-# Timestamp (TS) - commonly used for DCS release titles
-alias ts='date "+%Y-%m-%d %H:%M %Z"'
-
-# Timestamp No Delimiter (ND) - rarely used
-alias tsnd='date "+%Y%m%d%H%M%S"'
+# Timestamp (TS)
+alias ts='date -u "+%Y-%m-%dT%H:%M:%SZ"' # UTC, ISO-8601
+alias tslocal='date "+%Y-%m-%d %H:%M %Z"' # Local zone, minutes precision
+alias tsnd='date "+%Y%m%d%H%M%S"' # ND = No Delimiter
